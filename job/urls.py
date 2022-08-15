b3a9574eb56bebbd1f985a635dc3cc314a9b8d81
Ziad Dorrah <ziadadelhassan341@gmail.com>
2022-08-15 23:23:20 +0200

    urls & views

--- job/urls.py
+++ job/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from .models import job_list, job_detail
 urlpatterns = [
     path('', views.job_list),
-    path('<int:id>', views.job_detail),
+    path('<int:id> ', views.job_detail),
 ]
