b5667c94bb5c6fb3aebd603150ead335cfc1b788
Ziad Dorrah <ziadadelhassan341@gmail.com>
2022-08-15 21:01:46 +0200

    add job views & url

--- job/models.py
+++ job/models.py
@@ -17,6 +17,14 @@ class Job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
