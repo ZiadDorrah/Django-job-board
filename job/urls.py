797cac251dbf37d615ba45a8692ac6d22866edd2
Ziad Dorrah <ziadadelhassan341@gmail.com>
2022-08-15 23:37:08 +0200

    Error in job urls import

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
-from .models import job_list, job_detail
+from . import views
 urlpatterns = [
     path('', views.job_list),
-    path('<int:id> ', views.job_detail),
+    path('<int:id>', views.job_detail),
 ]
