1076fd281f14556f55d966a76dc52eaa230e0f69
Ziad Dorrah <ziadadelhassan341@gmail.com>
2022-08-25 00:14:23 +0200

    The Project is done

--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'job',
     "bootstrap4",
     'django_filters',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ziadadelhassan341@gmail.com'
+EMAIL_HOST_PASSWORD = 'ydpuvjnkjwklftkt'
+EMAIL_USE_TLS = 'True'
+EMAIL_PORT = '587'
